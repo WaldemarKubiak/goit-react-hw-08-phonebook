efceb26e570b960cfcae56d754594b3f330fa552
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-07-21 19:25:32 +0200

    Add persistore to store.js, add PersistGate to index.js , create auth folder with autFiles

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,52 @@
+import { login, logout, refreshUser, register } from './authOperations';
+
+const { createSlice } = require('@reduxjs/toolkit');
+
+const handleFulfilled = (state, action) => {
+  state.user = action.payload.user;
+  state.token = action.payload.token;
+  state.isLoggedIn = true;
+};
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    // [register.pending](state, action) {},
+    [register.fulfilled]: handleFulfilled,
+    // [register.rejected](state, action) {},
+
+    // [login.pending](state, action) {},
+    [login.fulfilled]: handleFulfilled,
+    // [login.rejected](state, action) {},
+
+    // [logout.pending](state, action) {},
+    [logout.fulfilled](state, action) {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLoggedIn = false;
+    },
+    // [logout.rejected](state, action) {},
+
+    [refreshUser.pending](state) {
+      state.isRefreshing = true;
+    },
+    [refreshUser.fulfilled](state, action) {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+      state.isRefreshing = false;
+    },
+    [refreshUser.rejected](state) {
+      state.isRefreshing = false;
+    },
+  },
+});
+
+export const authReducer = authSlice.reducer;
