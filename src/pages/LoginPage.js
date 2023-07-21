cf1f3dd12790ddeed62d4c723f02fee9b1056559
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-07-21 23:59:04 +0200

    Add  LoginForm, modify LoginPage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,3 +1,13 @@
+import { LoginForm } from 'components/LoginForm/LoginForm';
+import { Helmet } from 'react-helmet';
+
 export const LoginPage = () => {
-  return <div>Place for login page</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <LoginForm />
+    </div>
+  );
 };
